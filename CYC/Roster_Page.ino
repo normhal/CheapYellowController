09fbbb600418a6d4600a4e5e31cbbd64d271d3b7
Norman Halland <nhalland@telkomsa.net>
2025-04-28 15:49:34 +0200

    Add files via upload
    First Public Release

---
+++ CYC/Roster_Page.ino
@@ -0,0 +1,77 @@
+/* 
+ *****************************************************************************************************************************
+ *  COPYRIGHT (c) 2025 Norman Halland (NormHal@gmail.com) unless otherwise noted.
+ *
+ *  This program and all its associated modules is free software:
+ *  you can redistribute it and/or modify it under the terms of the
+ *  GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option)
+ *  any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses
+ *
+ *****************************************************************************************************************************
+*/
+static void tbl_roster_cb(lv_event_t * e)
+{
+  lv_obj_t * obj = lv_event_get_target(e);
+  uint16_t col;
+  uint16_t row;
+  lv_table_get_selected_cell(obj, &row, &col);
+//  Serial.printf("Selected Roster Row = %d, Col = %d\n", row, col);
+  if(rosterMode == EDIT_MODE)
+  {
+    editingID = row;
+    lv_textarea_set_text(objects.ta_name, locoName[editingID]);
+    lv_textarea_set_text(objects.ta_address, locoAddress[editingID]);
+
+    //Now set the functions
+    setupFuncEditSlots();
+    rosterMode = NORMAL_MODE;
+    callingPage = SCREEN_ID_ROSTER;
+    lv_obj_clear_state(objects.btn_edit_loco, LV_STATE_CHECKED);
+    loadScreen(SCREEN_ID_EDIT_LOCO);
+  }
+  if(rosterMode == SELECT_MODE)
+  {
+    selectedIDs[activeThrottle][activeSlot[activeThrottle]] = row;
+    populateThrottle();
+    rosterMode = NORMAL_MODE;
+    lv_obj_clear_state(objects.btn_select_loco, LV_STATE_CHECKED);
+    loadScreen(SCREEN_ID_THROTTLE);
+  }
+}
+
+void action_roster_button(lv_event_t * e)
+{
+  void *user_data = lv_event_get_user_data(e);
+  int pressedButton = *((int*)(&user_data));
+  switch(pressedButton)
+  {
+    case 28:
+      rosterMode = NORMAL_MODE;
+      break;
+    case 29:
+      rosterMode = NORMAL_MODE;
+      break;
+    case 30:       //Return to Calling Page
+      loadScreen(SCREEN_ID_MAIN);
+      break;
+    case 31:       //Edit Loco
+      callingPage = SCREEN_ID_ROSTER;
+      rosterMode = EDIT_MODE;
+      lv_obj_clear_state(objects.btn_select_loco, LV_STATE_CHECKED);
+//      loadScreen(SCREEN_ID_EDIT_LOCO);
+      break;
+    case 32:       //Select Loco
+      rosterMode = SELECT_MODE;
+      lv_obj_clear_state(objects.btn_edit_loco, LV_STATE_CHECKED);
+     break;
+    default:
+      break;
+  }
+}
---
+++ CYC/images.c
@@ -0,0 +1,7 @@
+#include "images.h"
+
+const ext_img_desc_t images[3] = {
+    { "DCCppEX", &img_dc_cpp_ex },
+    { "6", &img_6 },
+    { "X", &img_x },
+};
