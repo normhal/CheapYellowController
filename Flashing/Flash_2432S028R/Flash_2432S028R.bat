bb092e0cc550803929cbd1c4e8ef74b6c4ca4d90
Norman Halland <nhalland@telkomsa.net>
2025-05-14 16:28:05 +0200

    Delete Flashing directory

--- Flashing/Flash_2432S028R/Flash_2432S028R.bat
+++
@@ -1,5 +0,0 @@
-"esptool.exe" --chip esp32 --port "COM5" --baud 921600  --before default_reset --after hard_reset write_flash -e -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 "CYC.ino.bootloader.bin" 0x8000 "CYC.ino.partitions.bin" 0xe000 "boot_app0.bin" 0x10000 "CYC.ino.bin" 
-pause
-"mklittlefs.exe" -c "data" -p 256 -b 4096 -s 131072 "littlefs.bin" 
-pause
-"esptool.exe" --chip esp32 --port "COM5" --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 3997696 "littlefs.bin" 
