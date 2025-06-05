31eb8344cf7ab9c8bb5eeab86c3eb93e33be1780
Norman Halland <nhalland@telkomsa.net>
2025-06-05 20:26:09 +0200

    Delete Flashing directory
    All Flashing batch files will be replaced in due course

--- Flashing/Flash_3248W535C_102/Flash_3248W535C_102.bat
+++
@@ -1,5 +0,0 @@
-esptool.exe --chip esp32s3 --port COM35 --baud 921600  --before default_reset --after hard_reset write_flash  -z --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0 CYC.ino.bootloader.bin 0x8000 CYC.ino.partitions.bin 0xe000 boot_app0.bin 0x10000 CYC.ino.bin 
-mklittlefs.exe -c data -p 256 -b 4096 -s 917504 littlefs.bin 
-pause
-esptool.exe --chip esp32s3 --port COM35 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 3211264 littlefs.bin 
-pause
--- Flashing/Flash_4827S043R_102/Flash_4827S043R_102.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PORT=/dev/ttyUSB0
-ESPTOOL=~/.arduino15/packages/esp32/tools/esptool_py/4.9.dev3/esptool
-MKLITTLE=~/.arduino15/packages/esp32/tools/mklittlefs/3.0.0-gnu12-dc7f933/mklittlefs
-BAUD=460800
-$ESPTOOL --chip esp32s3 --port $PORT --baud $BAUD  --before default_reset --after hard_reset write_flash  -z --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0 CYC.ino.bootloader.bin 0x8000 CYC.ino.partitions.bin 0xe000 boot_app0.bin 0x10000 CYC.ino.bin 
-$MKLITTLE -c data -p 256 -b 4096 -s 917504 littlefs.bin
-$ESPTOOL --chip esp32s3 --port $PORT --baud $BAUD --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 3211264 littlefs.bin 
-#pause
